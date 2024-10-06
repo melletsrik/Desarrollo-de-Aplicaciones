1a7e470fd1047053858c69c23bd95db245386d65
Melissa <melkrischf@gmail.com>
2024-10-05 22:44:33 -0500

    correccion

--- Lab6_React/Ejercicios/E2/src/RutasPrivadas.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-// Componente RutaPrivada
-const RutaPrivada = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
-
-export default RutaPrivada;
