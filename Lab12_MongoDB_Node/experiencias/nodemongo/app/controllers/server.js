884762a185f1a9042ab781574fdd8c59a6545944
Ricardo Charahua Sánchez <ricardo.charahua@ucsm.edu.pe>
2024-11-27 16:31:55 -0500

    mongodb

---
+++ Lab12_MongoDB_Node/experiencias/nodemongo/app/controllers/server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+var db = require("./models/db");
+var Router = require("./controllers/routes");
+var port = 3000;
+app.use("/api", Router);
+app.listen(port);
+console.log("Ejecutandose en el puerto 3000");
---
+++ Lab12_MongoDB_Node/experiencias/nodemongo/app/models/products.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var ProductSchema = new Schema({
+  name: String,
+  amount: Number,
+  description: String,
+});
+module.exports = mongoose.model("products", ProductSchema);
